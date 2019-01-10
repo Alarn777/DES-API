a5b79192bee640fdc2d3de8e99bd0107f2d83f5a
michael rokitko <alarn777@gmail.com>
2019-01-10 14:01:02 +0200

    Start of work

--- main.cpp
+++ main.cpp
@@ -2,14 +2,31 @@
 #include <vector>
 #include <stdio.h>
 #include <bitset>
+#include<stdio.h>
+#include<stdint.h>
+typedef struct {
+    unsigned char TwoBits:2;
+} tTwoBits;
+typedef struct {
+    unsigned char FourBits:4;
+} tFourBits;
 
+typedef struct {
+    unsigned char SixBits:6;
+} tSixBits;
 
+typedef struct {
+    unsigned char FortyEightBits:48;
+} tFortyEightBitsBits;
 
 using namespace std;
 static unsigned char Positions[8] = {1,2,4,8,16,32,64,128};
 vector<unsigned char> encryptBlock(const vector<unsigned char>& block, const vector<unsigned char>& key);
 vector<unsigned char> decryptBlock(const vector<unsigned char>& block, const vector<unsigned char>& key);
 vector<vector<unsigned char>> generateKeys(const vector<unsigned char>& key,int& number_of_keys);
+vector<unsigned char>fiestel(vector<unsigned char> key, vector<unsigned char> block_48);
+
+
 
 int main() {
 
@@ -62,56 +79,7 @@ vector<unsigned char> encryptBlock(const vector<unsigned char>& block, const vec
 
 
 
-    //sbox configuration 6 bit to 4 bit
-    int s[8][4][16]=
-            {{
-                     14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7,
-                     0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8,
-                     4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0,
-                     15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13
-             },
-             {
-                     15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10,
-                     3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5,
-                     0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15,
-                     13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9
-             },
-             {
-                     10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8,
-                     13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1,
-                     13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7,
-                     1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12
-             },
-             {
-                     7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15,
-                     13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9,
-                     10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4,
-                     3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14
-             },
-             {
-                     2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9,
-                     14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6,
-                     4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14,
-                     11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3
-             },
-             {
-                     12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11,
-                     10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8,
-                     9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6,
-                     4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13
-             },
-             {
-                     4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1,
-                     13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6,
-                     1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2,
-                     6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12
-             },
-             {
-                     13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7,
-                     1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2,
-                     7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8,
-                     2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11
-             }};
+
     //straight pbox
     int per[32]={16 ,  7 , 20  ,21,
                  29 , 12 , 28 , 17,
@@ -199,7 +167,7 @@ vector<unsigned char> encryptBlock(const vector<unsigned char>& block, const vec
                                 24 ,   25 ,  26  ,  27  ,  28  , 29,
                                 28  ,  29  , 30,    31  ,  32  ,  1};
 
-    unsigned char left_block_48[48] = {};
+    unsigned char left_block_48[6] = {};
     cout << "Left block expanded" << endl;
     for (int j = 0; j < 48; ++j) {
         unsigned char temp_b = block_left[(half_block_to_48[j]-1) / 8];
@@ -212,10 +180,10 @@ vector<unsigned char> encryptBlock(const vector<unsigned char>& block, const vec
     }
     cout << endl;
 
-    unsigned char right_block_48[48] = {};
+    unsigned char right_block_48[6] = {};
     cout << "Right block expanded" << endl;
     for (int j = 0; j < 48; ++j) {
-        unsigned char temp_b = block_left[(half_block_to_48[j]-1) / 8];
+        unsigned char temp_b = block_right[(half_block_to_48[j]-1) / 8];
         if (temp_b & (1 << (7-((half_block_to_48[j]-1)%8)))) {
             cout << "1";
             right_block_48[j/8] |= 1UL << 7-(j%8);
@@ -227,13 +195,171 @@ vector<unsigned char> encryptBlock(const vector<unsigned char>& block, const vec
 
     unsigned char temp_xored_block[6] = {0};
 
-    for (int k = 0; k < 6; ++k) {
-        temp_xored_block[k] = right_block_48[k]^all_keys[0][k];
+//    for (int k = 0; k < 6; ++k) {
+//        temp_xored_block[k] = right_block_48[k]^all_keys[0][k];
+//    }
+
+    //actutal xoring for the blocks
+    for (int l = 0; l < 16; ++l) {
+        if(l%2 == 0)                                    // even
+        {
+//            for (int k = 0; k < 6; ++k) {
+//                temp_xored_block[k] = right_block_48[k]^all_keys[l][k];        //key number l
+//            }
+
+            vector<unsigned char> temp_block_vector;
+            temp_block_vector.reserve(6);
+            for (unsigned char i : right_block_48) {
+                temp_block_vector.push_back(i);
+
+            }
+            vector<unsigned char> temp = all_keys[0];
+            vector<unsigned char> temp_resoult_of_iteration = fiestel(all_keys[l],temp_block_vector);
+
+
+
+
+
+        } else {                                        //odd
+//            for (int k = 0; k < 6; ++k) {
+//                temp_xored_block[k] = right_block_48[k]^all_keys[l][k];        //key number l
+//            }
+
+        }
+
     }
+
+
+
+
+
 
     cout << endl;
 
 }
+
+
+vector<unsigned char> fiestel(vector<unsigned char> key, vector<unsigned char> block_48){
+    //sbox configuration 6 bit to 4 bit
+    int s[8][4][16]=
+            {{    //0
+                     14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7, //[0][0][n]
+                     0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8,
+                     4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0,
+                     15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13
+             },
+             {    //1
+                     15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10,
+                     3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5,
+                     0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15,
+                     13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9
+             },
+             {    //2
+                     10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8,
+                     13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1,
+                     13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7,
+                     1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12
+             },
+             {    //3
+                     7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15,
+                     13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9,
+                     10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4,
+                     3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14
+             },
+             {    //4
+                     2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9,
+                     14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6,
+                     4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14,
+                     11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3
+             },
+             {    //5
+                     12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11,
+                     10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8,
+                     9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6,
+                     4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13
+             },
+             {    //6
+                     4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1,
+                     13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6,
+                     1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2,
+                     6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12
+             },
+             {    //7
+                     13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7,
+                     1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2,
+                     7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8,
+                     2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11
+             }};
+    tSixBits block_of_6_bits[8];
+    tFortyEightBitsBits all_block = tFortyEightBitsBits();
+    for (int i = 0; i < 48; ++i) {
+        unsigned char temp_b = block_48[ i / 8];
+        if (temp_b & (1 << (7-(i%8)))) {
+            cout << "1";
+            all_block.FortyEightBits |= 1UL << 48-(i%8);
+        }
+        else
+            cout << "0";
+    }
+    cout << endl;
+
+
+
+//    for (int k = 0; k < 48; ++k) {
+//
+//        block_of_6_bits[0]
+//    }
+
+    cout << s[0][0][15]  << endl;
+    tTwoBits ayu;
+
+    unsigned char temp_b = block_48[0];
+    if (temp_b & (1 << (0))) {
+        cout << "1";
+        ayu.TwoBits |= 1UL << 0;
+    }
+    if (temp_b & (1 << (5))) {
+        cout << "1";
+        ayu.TwoBits |= 1UL << 1;
+    }
+
+    auto i1 = int(ayu.TwoBits);
+    cout << i1 << endl;
+//    for (int j = 0; j < 48; ++j) {
+//       int k_in_32_bit_block = 0;
+//        if( j < 6 ) {
+//            int row=(int(xorout[j+5])-48)+(int(xorout[j])-48)*2;
+//            int col= (int(xorout[j+1])-48)*8+(int(xorout[j+2])-48)*4+(int(xorout[j+3])-48)*2+(int(xorout[j+4])-48);
+//            int temp=s[j/6][row][col];
+//            unsigned char temp_b = block_right[(half_block_to_48[j] - 1) / 8];
+//            if (temp_b & (1 << (7 - ((half_block_to_48[j] - 1) % 8)))) {
+//               cout << "1";
+//               right_block_48[j / 8] |= 1UL << 7 - (j % 8);
+//           } else
+//               cout << "0";
+//       }
+//        if(6 <= j < 12){
+//
+//        }
+//    }
+
+
+
+
+
+    return key;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
